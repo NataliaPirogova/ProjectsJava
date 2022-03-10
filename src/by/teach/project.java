41e3b0192ac1b35b7170479604da5fd860468c60
NataliaPirogova <100864857+NataliaPirogova@users.noreply.github.com>
2022-03-10 21:12:50 +0300

    Update project.java

--- src/by/teach/project.java
+++ src/by/teach/project.java
@@ -2,6 +2,6 @@ package by.teach;
 
 public class project {
     public static void main(String[] args) {
-        System.out.println("Good evening");
+        System.out.println("Good evening, ladies");
     }
 }
